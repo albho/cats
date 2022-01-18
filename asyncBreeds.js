963a9c6318dc59d16d4e158afaeebe4b035fe45e
Albert Ho <albertcyi.ho@gmail.com>
2022-01-18 13:45:09 -0800

    Completed async testing

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,16 +1,24 @@
-// asyncBreeds.js
 const fs = require("fs");
 
-const breedDetailsFromFile = function (breed, done) {
+const breedDetailsFromFile = function (breed, functionToRunWhenThingsAreDone) {
   console.log("breedDetailsFromFile: Calling readFile...");
   fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
+    // CHANGE: Pass data into callback instead of returning it directly
     console.log("In readFile's Callback: it has the data.");
-    if (!error) done(data);
+    if (!error) {
+      functionToRunWhenThingsAreDone(data);
+    } else {
+      functionToRunWhenThingsAreDone(undefined);
+    }
   });
 };
 
-const printCatInfo = breed => {
-  console.log("Return Value: ", breed); //
+// CHANGE 1: Moved the console.log into a new function:
+const printOutCatBreed = breed => {
+  console.log("Return Value: ", breed); // => print out details correctly.
 };
 
-breedDetailsFromFile("Bombay", printCatInfo);
+// CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
+breedDetailsFromFile("Bombay", printOutCatBreed);
+
+module.exports = breedDetailsFromFile;
--- test/syncBreedsTest.js
+++ test/syncBreedsTest.js
@@ -7,7 +7,7 @@ describe("#breedDetails", () => {
       "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
 
     const actual = breedDetails("Bombay");
-    assert.equal(actual, expected);
+    assert.strictEqual(actual, expected);
   });
 
   it("returns breed details for the Balinese breed", () => {
@@ -15,12 +15,12 @@ describe("#breedDetails", () => {
       "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.";
 
     const actual = breedDetails("Balinese");
-    assert.equal(actual, expected);
+    assert.strictEqual(actual, expected);
   });
 
   it("returns undefined for a breed with no details", () => {
     const expected = undefined;
     const actual = breedDetails("Pug");
-    assert.equal(actual, expected);
+    assert.strictEqual(actual, expected);
   });
 });
